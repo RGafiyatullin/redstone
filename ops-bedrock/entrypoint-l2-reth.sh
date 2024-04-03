06a5e0f2d39b0b050c950ac524800dd2c8f194ad
Raman Hafiyatulin <r.gafiyatullin@me.com>
2024-04-03 20:34:27 +0300

    Checkpoint

--- ops-bedrock/entrypoint-l2-reth.sh
+++ ops-bedrock/entrypoint-l2-reth.sh
@@ -1,46 +1,45 @@
 #!/bin/sh
 set -exu
 
-VERBOSITY=${GETH_VERBOSITY:-3}
-GETH_DATA_DIR=/db
-GETH_CHAINDATA_DIR="$GETH_DATA_DIR/geth/chaindata"
+
+RETH_DATA_DIR=/db
+RETH_DATA_TOML="$RETH_DATA_DIR/reth.toml"
 GENESIS_FILE_PATH="${GENESIS_FILE_PATH:-/genesis.json}"
 CHAIN_ID=$(cat "$GENESIS_FILE_PATH" | jq -r .config.chainId)
 RPC_PORT="${RPC_PORT:-8545}"
 WS_PORT="${WS_PORT:-8546}"
+AUTH_RPC_PORT="${AUTH_RPC_PORT:-8551}"
 
-if [ ! -d "$GETH_CHAINDATA_DIR" ]; then
-	echo "$GETH_CHAINDATA_DIR missing, running init"
+if [ ! -f "$RETH_DATA_TOML" ]; then
+	echo "$RETH_DATA_TOML missing, running init"
 	echo "Initializing genesis."
 	op-reth init \
-		--datadir="$GETH_DATA_DIR" \
-		--chain="$GENESIS_FILE_PATH"
+		--datadir="$RETH_DATA_DIR" \
+		--chain="$GENESIS_FILE_PATH" || exit $?
 else
-	echo "$GETH_CHAINDATA_DIR exists."
+	echo "$RETH_DATA_TOML exists."
 fi
 
 # Warning: Archive mode is required, otherwise old trie nodes will be
 # pruned within minutes of starting the devnet.
 
-
-
 exec op-reth node \
-    --datadir="$GETH_DATA_DIR" \
+    --datadir="$RETH_DATA_DIR" \
 	--http \
 	--http.corsdomain="*" \
 	--http.addr=0.0.0.0 \
 	--http.port="$RPC_PORT" \
-	--http.api=all \
+	--http.api=web3,debug,eth,txpool,net \
 	--ws \
 	--ws.addr=0.0.0.0 \
 	--ws.port="$WS_PORT" \
 	--ws.origins="*" \
-	--ws.api=all \
+	--ws.api=debug,eth,txpool,net \
 	--disable-discovery \
 	--max-outbound-peers=0 \
     --max-inbound-peers=0 \
 	--authrpc.addr="0.0.0.0" \
-	--authrpc.port="8551" \
+	--authrpc.port="${AUTH_RPC_PORT}" \
 	--metrics 0.0.0.0:6060 \
     --chain="${GENESIS_FILE_PATH}" \
 	"$@"
